fa4498bd64315db1631d639bdca4642dcdeaaa7a
Andrew Ryu <andrewryu@outlook.com>
2022-12-12 21:34:51 -0800

    added deleteTransaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,13 @@
 // how we specify the application state changes in response to certain actions to our context
 export default (state, action) => {
-    switch (action.typoe) {
+    switch (action.type) {
+        case "DELETE_TRANSACTION":
+            return {
+                ...state,
+                transactions: state.transactions.filter(
+                    (transaction) => transaction.id !== action.payload
+                ),
+            };
         default:
             return state;
     }
