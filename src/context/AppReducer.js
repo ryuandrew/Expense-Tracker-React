9f103f41af6af6f5143cc53ecc94f1d2e3b8c6fb
Andrew Ryu <andrewryu@outlook.com>
2022-12-12 21:05:02 -0800

    added transaction component

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// how we specify the application state changes in response to certain actions to our context
+export default (state, action) => {
+    switch (action.typoe) {
+        default:
+            return state;
+    }
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// initial state
+// any global state goes in this object
+const initialState = {
+    transactions: [
+        { id: 1, text: "Costco", amount: -20 },
+        { id: 2, text: "Deposit", amount: 2500 },
+        { id: 3, text: "Fred Meyer", amount: -70 },
+        { id: 4, text: "Gas", amount: -30 },
+    ],
+};
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+        <GlobalContext.Provider value={{ transactions: state.transactions }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
