0a030414583464d1d223834fe02f6c634cf4830f
Andrew Ryu <andrewryu@outlook.com>
2022-12-12 21:20:44 -0800

    updated Balance and IncomeExpense Component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map((transaction) => transaction.amount);
+    console.log(amounts);
+
+    const total = amounts
+        .reduce((accumulator, item) => (accumulator += item), 0)
+        .toFixed(2);
+    console.log(total);
     return (
         <>
             <h4>YOUR BALANCE</h4>
-            <h1>$0.00</h1>
+            <h1>${total}</h1>
         </>
     );
 };
--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map((transaction) => transaction.amount);
+    console.log(amounts);
+
+    const income = amounts
+        .filter((item) => item > 0)
+        .reduce((accumulator, item) => (accumulator += item), 0)
+        .toFixed(2);
+    console.log(income);
+
+    const expense = amounts
+        .filter((item) => item < 0)
+        .reduce((accumulator, item) => (accumulator += item), 0)
+        .toFixed(2);
+    console.log(expense);
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>INCOME</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">{income}</p>
             </div>
             <div>
                 <h4>EXPENSE</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">{expense}</p>
             </div>
         </div>
     );
